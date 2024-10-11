5b17fac5f60ebbccb807ae4684897150827bcf8d
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-10-12 00:08:35 +0530

    Create Rotate_Array.cpp

---
+++ C++/QuestionsOfArrays/Rotate_Array.cpp
@@ -0,0 +1,28 @@
+/*
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n; // Ensure k is within the range [0, n)
+
+        // Reverse the entire array
+        reverse(nums.begin(), nums.end());
+        
+        // Reverse the first k elements
+        reverse(nums.begin(), nums.begin() + k);
+        
+        // Reverse the rest of the elements after k
+        reverse(nums.begin() + k, nums.end());
+    }
+};
