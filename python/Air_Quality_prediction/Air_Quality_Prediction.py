b7cbd17402618e35daf3915f1d65b65a643f6c9e
Ankurcr7 <ankurdas140905@gmail.com>
2024-10-11 23:11:34 +0530

    models

--- python/Air_Quality_prediction/Air_Quality_Prediction.py
+++ python/Air_Quality_prediction/Air_Quality_Prediction.py
@@ -1,6 +1,11 @@
 #import the required modules and classes
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+"""
+Note: Data cannot be in strings(text), it has to be in numbers(ex- 0,1,2...)
+"""
+
 
 
 # loading the dataset using pandas module
@@ -8,14 +13,15 @@ path_of_dataset = "Air_Quality.csv"
 train_data = pd.read_csv(path_of_dataset)
 
 # prints the first 5 rows of the dataset
-print(train_data.head())
+# print(train_data.head())
 
 # initializing the class RandomForestRegressor
 rfr = RandomForestRegressor()
 
 # creating models
+train_model = train_data.drop(['pollutant_avg'], axis=1) 
 target_value = train_data['pollutant_avg']
-train_model = train_data.drop('pollutant_avg', axis=1) 
 
+# returns the models which have to be input by user and predict the output in target value 
 print(train_model)
 print(target_value)
